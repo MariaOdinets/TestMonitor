f68a676be33eee9704871f3990b0ab12b2fa0bc0
Mary Odinets <mariaodinets@gmail.com>
2023-06-27 18:42:11 +0300

    initial framework git setup

---
+++ Pages/BasePage.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium;
+using TestMonitor.Utilities.Configuration;
+
+namespace TestMonitor.Pages
+{
+    public abstract class BasePage
+    {
+        protected IWebDriver Driver;
+        protected BasePage(IWebDriver driver)
+        {
+            Driver = driver;
+        }
+
+        public abstract bool IsPageOpened();
+        protected void OpenPageByURL()
+        {
+            Driver.Navigate().GoToUrl(Configurator.AppSettings.URL);
+        }
+    }
+}
---
+++ Utilities/Configuration/Configurator.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Reflection;
+
+namespace TestMonitor.Utilities.Configuration
+{
+    public static class Configurator
+    {
+        private static readonly Lazy<IConfiguration> configuration;
+        public static IConfiguration Configuration => configuration.Value;
+
+        static Configurator()
+        {
+            configuration = new Lazy<IConfiguration>(BuildConfiguration);
+        }
+
+        private static IConfiguration BuildConfiguration()
+        {
+            var basePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(basePath ?? throw new InvalidOperationException())
+                .AddJsonFile("appsettings.json"); ;
+
+            return builder.Build();
+        }
+
+        public static AppSettings AppSettings
+        {
+            get
+            {
+                var appSettings = new AppSettings();
+                var child = Configuration.GetSection("AppSettings");
+
+                appSettings.URL = child["URL"];
+
+                return appSettings;
+            }
+        }
+        public static string? BrowserType => Configuration[nameof(BrowserType)];
+    }
+}
