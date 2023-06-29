9274bcab0e7fe1f6b52116c1c695e27d311488ef
Mary Odinets <mariaodinets@gmail.com>
2023-06-29 16:24:20 +0300

    LoginPage

---
+++ Pages/LoginPage.cs
@@ -0,0 +1,46 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestMonitor.Pages
+{
+    public class LoginPage : BasePage
+    {
+        private static readonly By emailInput = By.Id("email");
+        private static readonly By passwordInput = By.Id("password");
+        private static readonly By loginButton = By.CssSelector(".button is-primary is-fullwidth");
+        private static readonly By rememberMeCheckbox = By.CssSelector(".b-checkbox checkbox");
+        private static readonly By errorMessage = By.CssSelector(".message is-danger");
+
+        public LoginPage(IWebDriver driver) : base(driver)
+        {
+            OpenPageByURL();
+        }
+
+        public override bool IsPageOpened()
+        {
+            return Driver.FindElement(loginButton).Displayed;
+        }
+
+        public void EnterEmail(string email)
+        {
+            Driver.FindElement(emailInput).SendKeys(email);
+        }
+
+        public void EnterPassword(string password)
+        {
+            Driver.FindElement(passwordInput).SendKeys(password);
+        }
+
+        public void ClickLogin()
+        {
+            Driver.FindElement(loginButton).Click();
+        }
+
+        public bool IsErrorMessageDisplayed()
+        {
+            return Driver.FindElement(errorMessage).Displayed;
+        }
+    }
+}
