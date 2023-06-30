84b919ca9a660703910a1349ea483ebe5040c94a
Mary Odinets <mariaodinets@gmail.com>
2023-06-30 17:24:18 +0300

    added BaseTest

---
+++ Tests/UI/BaseTest.cs
@@ -0,0 +1,31 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestMonitor.Core;
+using TestMonitor.Steps;
+
+namespace TestMonitor.Tests.UI
+{
+    public class BaseTest
+    {
+        protected IWebDriver Driver;
+        protected NavigationSteps NavigationSteps;
+
+        [SetUp]
+        public void SetUp()
+        {
+            Driver = new Browser().Driver;
+
+            NavigationSteps = new NavigationSteps(Driver);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Driver.Quit();
+        }
+    }
+}
