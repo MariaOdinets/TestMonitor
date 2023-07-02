531c856b2b6934183e88568aa706dfd0e7329f08
Mary Odinets <mariaodinets@gmail.com>
2023-07-02 15:18:48 +0300

    Allure

--- Tests/UI/BaseTest.cs
+++ Tests/UI/BaseTest.cs
@@ -1,18 +1,17 @@
-﻿using OpenQA.Selenium;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Allure.Commons;
+using NUnit.Allure.Core;
+using NUnit.Framework.Interfaces;
+using OpenQA.Selenium;
 using TestMonitor.Core;
 using TestMonitor.Steps;
-
 namespace TestMonitor.Tests.UI
 {
+    [AllureNUnit]
     public class BaseTest
     {
         protected IWebDriver Driver;
         protected NavigationSteps NavigationSteps;
+        private AllureLifecycle allure;
 
         [SetUp]
         public void SetUp()
@@ -20,11 +19,20 @@ namespace TestMonitor.Tests.UI
             Driver = new Browser().Driver;
 
             NavigationSteps = new NavigationSteps(Driver);
+            allure = AllureLifecycle.Instance;
         }
 
         [TearDown]
         public void TearDown()
         {
+            if (TestContext.CurrentContext.Result.Outcome.Status == TestStatus.Failed)
+            {
+                Screenshot screenshot = ((ITakesScreenshot)Driver).GetScreenshot();
+                byte[] screenshotBytes = screenshot.AsByteArray;
+
+                allure.AddAttachment("Screenshot", "image/png", screenshotBytes);
+            }
+
             Driver.Quit();
         }
     }
